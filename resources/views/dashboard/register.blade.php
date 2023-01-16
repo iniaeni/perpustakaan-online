afcb754c6191dc25a086a44c12c094bb01cc9659
sitinuraeni206 <sitinuraeni@smkwikrama.sch.id>
2023-01-16 19:17:19 +0700

    upload project

--- resources/views/dashboard/register.blade.php
+++ resources/views/dashboard/register.blade.php
@@ -20,54 +20,51 @@
             <div class="col-md-8 col-lg-6 col-xl-4 offset-xl-1">
               <form>
                 <div class="d-flex flex-row align-items-center justify-content-center justify-content-lg-start">
-                  <p class="lead fw-normal mb-0 me-3">Sign in with</p>
-                  <button type="button" class="btn btn-primary btn-floating mx-1">
-                    <i class="fab fa-facebook-f"></i>
-                  </button>
-      
-                  <button type="button" class="btn btn-primary btn-floating mx-1">
-                    <i class="fab fa-twitter"></i>
-                  </button>
-      
-                  <button type="button" class="btn btn-primary btn-floating mx-1">
-                    <i class="fab fa-linkedin-in"></i>
-                  </button>
-                </div>
-      
-                <div class="divider d-flex align-items-center my-4">
-                  <p class="text-center fw-bold mx-3 mb-0">Or</p>
+                  <h3 class="text center mb-5">Register</h3>
+                 
                 </div>
       
                 <!-- Email input -->
                 <div class="form-outline mb-4">
+                  <label class="form-label" for="form3Example3">Name</label>
+                  <input type="text" id="form3Example3" class="form-control form-control-lg"
+                    placeholder="Enter a valid email address" />
+                  
+                </div>
+                <div class="form-outline mb-4">
+                  <label class="form-label" for="form3Example3">Email address</label>
                   <input type="email" id="form3Example3" class="form-control form-control-lg"
                     placeholder="Enter a valid email address" />
-                  <label class="form-label" for="form3Example3">Email address</label>
+                  
+                </div>
+                <div class="form-outline mb-4">
+                  <label class="form-label" for="form3Example3">Domisili</label>
+                  <input type="text" id="form3Example3" class="form-control form-control-lg"
+                    placeholder="Enter a valid email address" />
+                  
+                </div>
+                <div class="form-outline mb-4">
+                  <label class="form-label" for="form3Example3">No Telepon</label>
+                  <input type="text" id="form3Example3" class="form-control form-control-lg"
+                    placeholder="Enter a valid email address" />
+                  
                 </div>
       
                 <!-- Password input -->
                 <div class="form-outline mb-3">
+                  <label class="form-label" for="form3Example4">Password</label>
                   <input type="password" id="form3Example4" class="form-control form-control-lg"
                     placeholder="Enter password" />
-                  <label class="form-label" for="form3Example4">Password</label>
+                 
                 </div>
       
-                <div class="d-flex justify-content-between align-items-center">
-                  <!-- Checkbox -->
-                  <div class="form-check mb-0">
-                    <input class="form-check-input me-2" type="checkbox" value="" id="form2Example3" />
-                    <label class="form-check-label" for="form2Example3">
-                      Remember me
-                    </label>
-                  </div>
-                  <a href="#!" class="text-body">Forgot password?</a>
-                </div>
+               
       
                 <div class="text-center text-lg-start mt-4 pt-2">
                   <button type="button" class="btn btn-primary btn-lg"
                     style="padding-left: 2.5rem; padding-right: 2.5rem;">Login</button>
-                  <p class="small fw-bold mt-2 pt-1 mb-0">Don't have an account? <a href="#!"
-                      class="link-danger">Register</a></p>
+                  <p class="small fw-bold mt-2 pt-1 mb-0">Have an account? <a href="#!"
+                      class="link-danger">Login</a></p>
                 </div>
       
               </form>
